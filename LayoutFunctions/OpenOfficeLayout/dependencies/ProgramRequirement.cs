d6a9a423f669a8b921ddaa3fedb2619511a5a851
Andrew Heumann <andrew@hypar.io>
2022-09-29 12:26:04 -0400

    fix build

---
+++ LayoutFunctions/OpenOfficeLayout/dependencies/ProgramRequirement.cs
@@ -0,0 +1,33 @@
+using System;
+using Newtonsoft.Json;
+
+namespace Elements
+{
+    public partial class ProgramRequirement
+    {
+        public int CountPlaced { get; set; }
+        public int RemainingToPlace
+        {
+            get
+            {
+                return this.SpaceCount - this.CountPlaced;
+            }
+        }
+
+        [JsonProperty("Qualified Program Name")]
+        public string QualifiedProgramName => String.IsNullOrWhiteSpace(this.ProgramGroup) ? this.ProgramName : $"{this.ProgramGroup} - {this.ProgramName}";
+
+        public double GetAreaPerSpace()
+        {
+            if (this.AreaPerSpace != 0)
+            {
+                return this.AreaPerSpace;
+            }
+            if (this.Width != null && this.Depth != null && this.Width != 0 && this.Depth != 0)
+            {
+                return this.Width.Value * this.Depth.Value;
+            }
+            return 0;
+        }
+    }
+}
