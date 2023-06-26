9387f8fc46a631ee1c1f066a7cd17c362730deda
AMCBRIDGE\ksloboda <ksloboda@amcbridge.com>
2023-06-26 16:57:03 +0300

    add phone booth tests

--- LayoutFunctions/PhoneBoothLayout/test/PhoneBoothLayoutTests.cs
+++ LayoutFunctions/PhoneBoothLayout/test/PhoneBoothLayoutTests.cs
@@ -14,6 +14,7 @@ namespace PhoneBoothLayout.Tests
     {
         private const string INPUT = "../../../_input/";
         private const string OUTPUT = "../../../_output/";
+        private string[] orderedKeys = new[] { "Configuration B" };
 
         [Fact]
         public void PhoneBoothConfigurations()
@@ -24,13 +25,13 @@ namespace PhoneBoothLayout.Tests
 
             var (output, spacePlanningModel) = PhoneBoothLayoutTest(testName);
             var elements = output.Model.AllElementsOfType<ElementInstance>();
-            var boundaries = spacePlanningModel.AllElementsOfType<SpaceBoundary>().Where(z => z.Name == "Phone Booth");
+            var boundaries = spacePlanningModel.AllElementsOfType<SpaceBoundary>().Where(z => z.Name == "Phone Booth").OrderBy(b => b.Boundary.Perimeter.Center().Y).ToList();
 
-            foreach (var boundary in boundaries)
+            for (int i = 0; i < orderedKeys.Count(); i++)
             {
-                var depth = boundary.Bounds.XSize;
-                var config = configs.FirstOrDefault(c => c.Value.Depth.ApproximatelyEquals(depth, 0.3) && c.Value.Depth < depth).Value;
-                Assert.NotNull(config);
+                var boundary = boundaries[i];
+                var config = configs.FirstOrDefault(c => c.Key == orderedKeys[i]).Value;
+                Assert.True(config.Width < boundary.Bounds.YSize);
 
                 var offsetedBox = boundary.Bounds.Offset(0.1);
                 var boundaryElements = elements.Where(e => offsetedBox.Contains(e.Transform.Origin)).ToList();
@@ -48,17 +49,20 @@ namespace PhoneBoothLayout.Tests
         {
             var spacePlanningModel = Model.FromJson(System.IO.File.ReadAllText($"{INPUT}/{testName}/Space Planning Zones.json"));
             var levelsModel = Model.FromJson(System.IO.File.ReadAllText($"{INPUT}/{testName}/Levels.json"));
+            var circulationModel = Model.FromJson(System.IO.File.ReadAllText($"{INPUT}/{testName}/Circulation.json"));
             var input = GetInput(testName);
             var output = PhoneBoothLayout.Execute(
                 new Dictionary<string, Model>
                 {
                     {"Space Planning Zones", spacePlanningModel},
-                    {"Levels", levelsModel}
+                    {"Levels", levelsModel},
+                    {"Circulation", circulationModel},
                 }, input);
 
             System.IO.File.WriteAllText($"{OUTPUT}/{testName}/PhoneBoothLayout.json", output.Model.ToJson());
             output.Model.AddElements(spacePlanningModel.Elements.Values);
             output.Model.AddElements(levelsModel.Elements.Values);
+            output.Model.AddElements(circulationModel.Elements.Values);
             output.Model.ToGlTF($"{OUTPUT}/{testName}/PhoneBoothLayout.glb");
             output.Model.ToGlTF($"{OUTPUT}/{testName}/PhoneBoothLayout.gltf", false);
 
