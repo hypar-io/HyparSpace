d66dd8bbbf9304ca3a1f283d08923fab748bdede
AMCBRIDGE\ksloboda <ksloboda@amcbridge.com>
2023-07-03 14:55:46 +0300

    add tests to open office

--- LayoutFunctions/OpenOfficeLayout/test/OpenOfficeLayoutTests.cs
+++ LayoutFunctions/OpenOfficeLayout/test/OpenOfficeLayoutTests.cs
@@ -3,10 +3,10 @@ using Xunit;
 using System.IO;
 using System.Collections.Generic;
 using Elements.Serialization.glTF;
-using Newtonsoft.Json;
-using Elements.Components;
 using System.Linq;
 using LayoutFunctionCommon;
+using System;
+using Elements.Geometry;
 
 namespace OpenOfficeLayout.Tests
 {
@@ -18,51 +18,82 @@ namespace OpenOfficeLayout.Tests
         [Fact]
         public void OpenOfficeConfigurations()
         {
-            // test with one room for each configuration
             var testName = "Configurations";
-            var configs = LayoutStrategies.GetConfigurations("OpenOfficeConfigurations.json");
+            var configs = LayoutStrategies.GetConfigurations("OpenOfficeDeskConfigurations.json");
+            var spacePlanningModel = Model.FromJson(System.IO.File.ReadAllText($"{INPUT}/{testName}/Space Planning Zones.json"));
+            var levelsModel = Model.FromJson(System.IO.File.ReadAllText($"{INPUT}/{testName}/Levels.json"));
+            var circulationModel = Model.FromJson(System.IO.File.ReadAllText($"{INPUT}/{testName}/Circulation.json"));
+            var columnsModel = Model.FromJson(System.IO.File.ReadAllText($"{INPUT}/{testName}/Columns.json"));
+            var input = GetInput(testName);
 
-            var (output, spacePlanningModel) = OpenOfficeLayoutTest(testName);
-            var elements = output.Model.AllElementsOfType<ElementInstance>();
-            var boundaries = spacePlanningModel.AllElementsOfType<SpaceBoundary>().Where(z => z.Name == "Open Office");
-
-            foreach (var boundary in boundaries)
+            foreach (var columnAvoidanceStrategy in Enum.GetNames(typeof(OpenOfficeLayoutInputsColumnAvoidanceStrategy)))
             {
-                var depth = boundary.Bounds.XSize;
-                var config = configs.FirstOrDefault(c => c.Value.Depth.ApproximatelyEquals(depth, 0.3) && c.Value.Depth < depth).Value;
-                Assert.NotNull(config);
+                var output = OpenOfficeLayoutTest(testName, columnAvoidanceStrategy, spacePlanningModel, levelsModel, circulationModel, columnsModel, input);
+                var elements = output.Model.AllElementsAssignableFromType<ElementInstance>().Where(e => e.BaseDefinition is not Column).ToList();
+                var openCollabBoundaries = output.Model.AllElementsOfType<SpaceBoundary>().Where(b => b.Name == "Open Collaboration");
+                var boundaries = spacePlanningModel.AllElementsOfType<SpaceBoundary>().Where(b => b.Name == "Open Office").OrderBy(b => b.Boundary.Perimeter.Center().Y).ThenBy((b => b.Boundary.Perimeter.Center().X)).ToList();
+                var expectedResults = GetTestResults(testName, columnAvoidanceStrategy.Replace("_", ""));
 
-                var offsetedBox = boundary.Bounds.Offset(0.1);
-                var boundaryElements = elements.Where(e => offsetedBox.Contains(e.Transform.Origin)).ToList();
-                
-                foreach (var contentItem in config.ContentItems)
+                for (int i = 0; i < boundaries.Count(); i++)
                 {
-                    var boundaryElement = boundaryElements.FirstOrDefault(be => be.Name == contentItem.Name);
-                    Assert.NotNull(boundaryElement);
-                    boundaryElements.Remove(boundaryElement);
+                    var boundary = boundaries[i];
+                    var expectedResult = expectedResults[i];
+                    var config = configs.FirstOrDefault(c => c.Key == expectedResult.DeskType).Value;
+                    Assert.NotNull(config);
+
+                    var offsetedBox = boundary.Bounds.Offset(0.1);
+                    offsetedBox.Min -= new Vector3(0, 0, 0.1);
+                    var boundaryElements = elements.Where(e => offsetedBox.Contains(e.Transform.Origin)).ToList();
+
+                    // Check items
+                    foreach (var contentItem in config.ContentItems)
+                    {
+                        var suitableElements = boundaryElements.Where(be => be.AdditionalProperties.TryGetValue("gltfLocation", out var gltfLocation) && gltfLocation.ToString() == contentItem.Url).ToList();
+                        Assert.True(suitableElements.Count() >= expectedResult.Count);
+
+                        var expectedElements = suitableElements.SkipLast(suitableElements.Count() - expectedResult.Count);
+                        boundaryElements.RemoveAll(b => expectedElements.Contains(b));
+                    }
+
+                    // Check open collab areas
+                    var suitableCollabBoundaries = openCollabBoundaries.Where(b => boundary.Boundary.Perimeter.Contains(b.Boundary.Perimeter.Centroid()));
+                    Assert.Equal(suitableCollabBoundaries.Count(), expectedResult.CollabCount);
+                    Assert.True(suitableCollabBoundaries.Sum(b => b.Area).ApproximatelyEquals(expectedResult.CollabArea, 0.2));
                 }
             }
         }
 
-        private (OpenOfficeLayoutOutputs output, Model spacePlanningModel) OpenOfficeLayoutTest(string testName)
+        private OpenOfficeLayoutOutputs OpenOfficeLayoutTest(
+            string testName, 
+            string columnAvoidanceStrategyName,
+            Model spacePlanningModel,
+            Model levelsModel,
+            Model circulationModel,
+            Model columnsModel,
+            OpenOfficeLayoutInputs input)
         {
-            var spacePlanningModel = Model.FromJson(System.IO.File.ReadAllText($"{INPUT}/{testName}/Space Planning Zones.json"));
-            var levelsModel = Model.FromJson(System.IO.File.ReadAllText($"{INPUT}/{testName}/Levels.json"));
-            var input = GetInput(testName);
+            Enum.TryParse(columnAvoidanceStrategyName, out OpenOfficeLayoutInputsColumnAvoidanceStrategy columnAvoidanceStrategy);
+            input.ColumnAvoidanceStrategy = columnAvoidanceStrategy;
+
+            ElementProxy.ClearCache();
             var output = OpenOfficeLayout.Execute(
                 new Dictionary<string, Model>
                 {
                     {"Space Planning Zones", spacePlanningModel},
-                    {"Levels", levelsModel}
+                    {"Levels", levelsModel},
+                    {"Circulation", circulationModel},
+                    {"Columns", columnsModel},
                 }, input);
 
-            System.IO.File.WriteAllText($"{OUTPUT}/{testName}/OpenOfficeLayout.json", output.Model.ToJson());
+            System.IO.File.WriteAllText($"{OUTPUT}/{testName}/OpenOfficeLayout_" + columnAvoidanceStrategyName + ".json", output.Model.ToJson());
             output.Model.AddElements(spacePlanningModel.Elements.Values);
             output.Model.AddElements(levelsModel.Elements.Values);
-            output.Model.ToGlTF($"{OUTPUT}/{testName}/OpenOfficeLayout.glb");
-            output.Model.ToGlTF($"{OUTPUT}/{testName}/OpenOfficeLayout.gltf", false);
+            output.Model.AddElements(circulationModel.Elements.Values);
+            output.Model.AddElements(columnsModel.Elements.Values);
+            output.Model.ToGlTF($"{OUTPUT}/{testName}/OpenOfficeLayout_" + columnAvoidanceStrategyName + ".glb");
+            output.Model.ToGlTF($"{OUTPUT}/{testName}/OpenOfficeLayout_" + columnAvoidanceStrategyName + ".gltf", false);
 
-            return (output, spacePlanningModel);
+            return output;
         }
 
         private OpenOfficeLayoutInputs GetInput(string testName)
@@ -70,5 +101,24 @@ namespace OpenOfficeLayout.Tests
             var json = File.ReadAllText($"{INPUT}/{testName}/inputs.json");
             return Newtonsoft.Json.JsonConvert.DeserializeObject<OpenOfficeLayoutInputs>(json);
         }
+
+        private List<TestResult> GetTestResults(string testName, string resultName)
+        {
+            var json = File.ReadAllText($"{INPUT}/{testName}/{resultName}Results.json");
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<TestResults>(json).Results;
+        }
+    }
+
+    public class TestResult
+    {
+        public string DeskType { get; set; }
+        public int Count { get; set; }
+        public double CollabArea { get; set; }
+        public int CollabCount { get; set; }
+    }
+
+    public class TestResults
+    {
+        public List<TestResult> Results { get; set; }
     }
 }
