81e91866b55b721c286c2f0ec44c487f5396320f
AMCBRIDGE\ksloboda <ksloboda@amcbridge.com>
2023-07-03 17:08:34 +0300

    add tests to open collab

--- LayoutFunctions/OpenCollabLayout/test/OpenCollaborationLayoutTests.cs
+++ LayoutFunctions/OpenCollabLayout/test/OpenCollaborationLayoutTests.cs
@@ -18,49 +18,56 @@ namespace OpenCollaborationLayout.Tests
         [Fact]
         public void OpenCollaborationConfigurations()
         {
-            // test with one room for each configuration
             var testName = "Configurations";
             var configs = LayoutStrategies.GetConfigurations("OpenCollaborationConfigurations.json");
+            var levelsModel = Model.FromJson(System.IO.File.ReadAllText($"{INPUT}/{testName}/Levels.json"));
+            var circulationModel = Model.FromJson(System.IO.File.ReadAllText($"{INPUT}/{testName}/Circulation.json"));
+            var input = GetInput(testName);
 
-            var (output, spacePlanningModel) = OpenCollaborationLayoutTest(testName);
-            var elements = output.Model.AllElementsOfType<ElementInstance>();
-            var boundaries = spacePlanningModel.AllElementsOfType<SpaceBoundary>().Where(z => z.Name == "Open Collaboration");
-
-            foreach (var boundary in boundaries)
+            foreach (var config in configs)
             {
-                var depth = boundary.Bounds.XSize;
-                var config = configs.FirstOrDefault(c => c.Value.Depth.ApproximatelyEquals(depth, 0.3) && c.Value.Depth < depth).Value;
-                Assert.NotNull(config);
+                var (output, spacePlanningModel) = OpenCollaborationLayoutTest(testName, config.Key, levelsModel, circulationModel, input);
+                var elements = output.Model.AllElementsOfType<ElementInstance>();
+                var boundary = spacePlanningModel.AllElementsOfType<SpaceBoundary>().Where(z => z.Name == "Open Collaboration").OrderBy(b => b.Boundary.Perimeter.Center().Y).First();
+                
+                Assert.True(config.Value.Depth < boundary.Bounds.XSize && config.Value.Width < boundary.Bounds.YSize);
 
                 var offsetedBox = boundary.Bounds.Offset(0.1);
                 var boundaryElements = elements.Where(e => offsetedBox.Contains(e.Transform.Origin)).ToList();
-                
-                foreach (var contentItem in config.ContentItems)
+
+                foreach (var contentItem in config.Value.ContentItems)
                 {
-                    var boundaryElement = boundaryElements.FirstOrDefault(be => be.Name == contentItem.Name);
+                    var boundaryElement = boundaryElements.FirstOrDefault(be => be.AdditionalProperties.TryGetValue("gltfLocation", out var gltfLocation) && gltfLocation.ToString() == contentItem.Url);
                     Assert.NotNull(boundaryElement);
                     boundaryElements.Remove(boundaryElement);
                 }
             }
         }
 
-        private (OpenCollaborationLayoutOutputs output, Model spacePlanningModel) OpenCollaborationLayoutTest(string testName)
+        private (OpenCollaborationLayoutOutputs output, Model spacePlanningModel) OpenCollaborationLayoutTest(
+            string testName, 
+            string configName, 
+            Model levelsModel, 
+            Model circulationModel,
+            OpenCollaborationLayoutInputs input)
         {
-            var spacePlanningModel = Model.FromJson(System.IO.File.ReadAllText($"{INPUT}/{testName}/Space Planning Zones.json"));
-            var levelsModel = Model.FromJson(System.IO.File.ReadAllText($"{INPUT}/{testName}/Levels.json"));
-            var input = GetInput(testName);
+            ElementProxy.ClearCache();
+
+            var spacePlanningModel = Model.FromJson(System.IO.File.ReadAllText($"{INPUT}/{testName}/Space Planning Zones_" + configName + ".json"));
             var output = OpenCollaborationLayout.Execute(
                 new Dictionary<string, Model>
                 {
                     {"Space Planning Zones", spacePlanningModel},
-                    {"Levels", levelsModel}
+                    {"Levels", levelsModel},
+                    {"Circulation", circulationModel},
                 }, input);
 
-            System.IO.File.WriteAllText($"{OUTPUT}/{testName}/OpenCollaborationLayout.json", output.Model.ToJson());
+            System.IO.File.WriteAllText($"{OUTPUT}/{testName}/OpenCollaborationLayout_" + configName + ".json", output.Model.ToJson());
             output.Model.AddElements(spacePlanningModel.Elements.Values);
             output.Model.AddElements(levelsModel.Elements.Values);
-            output.Model.ToGlTF($"{OUTPUT}/{testName}/OpenCollaborationLayout.glb");
-            output.Model.ToGlTF($"{OUTPUT}/{testName}/OpenCollaborationLayout.gltf", false);
+            output.Model.AddElements(circulationModel.Elements.Values);
+            output.Model.ToGlTF($"{OUTPUT}/{testName}/OpenCollaborationLayout_" + configName + ".glb");
+            output.Model.ToGlTF($"{OUTPUT}/{testName}/OpenCollaborationLayout_" + configName + ".gltf", false);
 
             return (output, spacePlanningModel);
         }
