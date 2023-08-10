e9d3fb80ba743262267c5550ff9e07d3c24c353c
AMCBRIDGE\ksloboda <ksloboda@amcbridge.com>
2023-08-10 16:51:38 +0300

    update catalog

--- LayoutFunctions/LayoutFunctionCommon/Configurations.cs
+++ LayoutFunctions/LayoutFunctionCommon/Configurations.cs
@@ -1,7 +1,11 @@
+using Elements;
 using Elements.Components;
 using Elements.Geometry;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace LayoutFunctionCommon
 {
@@ -9,34 +13,36 @@ namespace LayoutFunctionCommon
     {
         private static SpaceConfiguration _originalConfigs { get; set; }
         private static SpaceConfiguration _yFlippedConfigs { get; set; }
-        private static SpaceConfiguration _xFlippedConfigs { get; set; }
-        private static SpaceConfiguration _xyFlippedConfigs { get; set; }
 
         public static SpaceConfiguration OriginalConfigs { get { return _originalConfigs; } }
         public static SpaceConfiguration YFlippedConfigs { get { return _yFlippedConfigs; } }
-        public static SpaceConfiguration XFlippedConfigs { get { return _xFlippedConfigs; } }
-        public static SpaceConfiguration XYFlippedConfigs { get { return _xyFlippedConfigs; } }
 
         public static void Init(SpaceConfiguration configs)
         {
             _originalConfigs = configs;
             _yFlippedConfigs = GetFlippedConfigs(configs);
-            _xFlippedConfigs = GetRotated180Configs(_yFlippedConfigs);
-            _xyFlippedConfigs = GetRotated180Configs(configs);
         }
 
         public static (SpaceConfiguration Configs, Transform Transform) GetConfigs(Vector3 centroid, bool hFlip, bool vFlip)
         {
             var newTransform = new Transform();
             newTransform.RotateAboutPoint(centroid, Vector3.ZAxis, vFlip ? 180 : 0);
-                            
+
             return ((hFlip ^ vFlip ? _yFlippedConfigs : _originalConfigs), newTransform);
         }
 
         public static SpaceConfiguration GetFlippedConfigs(SpaceConfiguration configs)
         {
+            var mirroredNames = ContentCatalogRetrieval.GetCatalog().Content
+                .Where(c => c.Name.Contains("Mirrored"))
+                .ToDictionary(me => me.Name.Replace(" Mirrored", ""), me => me);
             Action<ContentConfiguration.ContentItem, Vector3, Vector3> flip = (item, min, max) =>
             {
+                if (item.Name.Contains("Plinth Base - Pedestal - Mobile - Wood Top"))
+                {
+
+                }
+
                 // Mirror origin
                 var newOrigin = item.Transform.Origin;
                 newOrigin.Y = max.Y - (item.Transform.Origin.Y - min.Y);
@@ -47,7 +53,17 @@ namespace LayoutFunctionCommon
                 item.Transform.RotateAboutPoint(item.Transform.Origin, Vector3.ZAxis, item.Transform.YAxis.PlaneAngleTo(flippedY));
 
                 // Move origin relative to the width of the object
-                newOrigin += item.Transform.XAxis.Negate() * (item.ContentElement.BoundingBox.Max.X - Math.Abs(item.ContentElement.BoundingBox.Min.X));
+                if (mirroredNames.TryGetValue(item.Name, out var mirroredElement) && mirroredElement != null)
+                {
+                    item.Name = mirroredElement.Name;
+                    item.Url = mirroredElement.GltfLocation;
+                    newOrigin += item.Transform.XAxis.Negate() * (item.ContentElement.BoundingBox.Max.X - Math.Abs(item.ContentElement.BoundingBox.Min.X));
+                }
+                else
+                {
+                    newOrigin += item.Transform.XAxis.Negate() * (item.ContentElement.BoundingBox.Max.X - Math.Abs(item.ContentElement.BoundingBox.Min.X));
+                }
+
                 item.Transform.Matrix.SetTranslation(newOrigin);
 
                 item.Anchor = new Vector3(item.Anchor.X, -item.Anchor.Y, item.Anchor.Z);
@@ -56,16 +72,16 @@ namespace LayoutFunctionCommon
             return GetNewConfigs(configs, flip);
         }
 
-        public static SpaceConfiguration GetRotated180Configs(SpaceConfiguration configs)
-        {
-            Action<ContentConfiguration.ContentItem, Vector3, Vector3> rotate = (item, min, max) =>
-            {
-                item.Transform.RotateAboutPoint((min + max) / 2, Vector3.ZAxis, 180);
-                item.Anchor = item.Anchor.Negate();
-            };
+        // public static SpaceConfiguration GetRotated180Configs(SpaceConfiguration configs)
+        // {
+        //     Action<ContentConfiguration.ContentItem, Vector3, Vector3> rotate = (item, min, max) =>
+        //     {
+        //         item.Transform.RotateAboutPoint((min + max) / 2, Vector3.ZAxis, 180);
+        //         item.Anchor = item.Anchor.Negate();
+        //     };
 
-            return GetNewConfigs(configs, rotate);
-        }
+        //     return GetNewConfigs(configs, rotate);
+        // }
 
         public static SpaceConfiguration GetNewConfigs(SpaceConfiguration configs, Action<ContentConfiguration.ContentItem, Vector3, Vector3> change)
         {
@@ -83,8 +99,6 @@ namespace LayoutFunctionCommon
                     ContentItems = new List<ContentConfiguration.ContentItem>()
                 };
 
-                var min = config.Value.CellBoundary.Min;
-                var max = config.Value.CellBoundary.Max;
                 foreach (var item in config.Value.ContentItems)
                 {
                     var newItem = new ContentConfiguration.ContentItem()
@@ -95,8 +109,7 @@ namespace LayoutFunctionCommon
                         Anchor = item.Anchor,
                     };
 
-                    change(newItem, min, max);
-
+                    change(newItem, config.Value.CellBoundary.Min, config.Value.CellBoundary.Max);
                     newConfig.ContentItems.Add(newItem);
                 }
 
