899f3f58e8487ccd2157a41f2e0ecaad1b26c9d9
Andrew <andrew@hypar.io>
2023-09-07 09:05:32 -0400

    create walls for meeting rooms

--- LayoutFunctions/MeetingRoomLayout/src/Function.g.cs
+++ LayoutFunctions/MeetingRoomLayout/src/Function.g.cs
@@ -61,11 +61,11 @@ namespace MeetingRoomLayout
             Console.WriteLine($"Time to load assemblies: {sw.Elapsed.TotalSeconds})");
 
             if(this.store == null)
-            {
+            { 
                 this.store = new S3ModelStore<MeetingRoomLayoutInputs>(RegionEndpoint.GetBySystemName("us-west-1"));
             }
 
-            var l = new InvocationWrapper<MeetingRoomLayoutInputs,MeetingRoomLayoutOutputs>(store, MeetingRoomLayout.Execute);
+            var l = new InvocationWrapper<MeetingRoomLayoutInputs,MeetingRoomLayoutOutputs> (store, MeetingRoomLayout.Execute);
             var output = await l.InvokeAsync(args);
             return output;
         }
