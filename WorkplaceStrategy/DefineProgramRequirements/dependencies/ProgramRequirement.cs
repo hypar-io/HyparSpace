b4c9fa43a3fe96a882fb8b3bafdff5598f39f559
Andrew <andrew@hypar.io>
2024-04-23 10:49:59 -0400

    program requirement id fixes

--- WorkplaceStrategy/DefineProgramRequirements/dependencies/ProgramRequirement.cs
+++ WorkplaceStrategy/DefineProgramRequirements/dependencies/ProgramRequirement.cs
@@ -8,11 +8,18 @@ namespace Elements
     {
         [JsonProperty("Qualified Program Name")]
         public string QualifiedProgramName => String.IsNullOrEmpty(this.ProgramGroup) ? this.ProgramName : $"{this.ProgramGroup} - {this.ProgramName}";
+       
+       [JsonProperty("Program Display Name")]
+        public string ProgramDisplayName { get; set; }
+
         public bool Enclosed { get; set; }
         public ProfileConstraint Dimensions { get; internal set; }
 
         [JsonProperty("Default Wall Type")]
         [Newtonsoft.Json.JsonConverter(typeof(Newtonsoft.Json.Converters.StringEnumConverter))]
         public ProgramRequirementsDefaultWallType? DefaultWallType { get; internal set; }
+
+        [JsonProperty("Layout Type")]
+        public InputFolder LayoutType { get; set; }
     }
 }
--- WorkplaceStrategy/DefineProgramRequirements/dependencies/ProgramRequirements.cs
+++ WorkplaceStrategy/DefineProgramRequirements/dependencies/ProgramRequirements.cs
@@ -1,15 +1,20 @@
 using Elements.Geometry;
 using Newtonsoft.Json;
 using System;
+using System.Collections;
 namespace DefineProgramRequirements;
 
 public partial class ProgramRequirements
 {
     public string QualifiedProgramName => String.IsNullOrEmpty(this.ProgramGroup) ? this.ProgramName : $"{this.ProgramGroup} - {this.ProgramName}";
 
+    public Guid? Id { get; set; }
+
+    [JsonProperty("Program Display Name")]
+    public string ProgramDisplayName { get; set; }
     public Elements.ProgramRequirement ToElement(Elements.CatalogWrapper catalogWrapper, Elements.SpaceConfigurationElement spaceConfigElem)
     {
-        return new Elements.ProgramRequirement
+        var req = new Elements.ProgramRequirement
         {
             ProgramGroup = this.ProgramGroup,
             ProgramName = this.ProgramName,
@@ -24,8 +29,12 @@ public partial class ProgramRequirements
             Catalog = catalogWrapper?.Id,
             Enclosed = this.Enclosed ?? false,
             SpaceConfig = spaceConfigElem?.Id,
-            DefaultWallType = this.DefaultWallType
+            DefaultWallType = this.DefaultWallType,
+            ProgramDisplayName = this.ProgramDisplayName,
+            LayoutType = this.LayoutType?.Id is not null ? new InputFolder(this.LayoutType.Id, Array.Empty<InputFileRef>(), this.LayoutType.Name) : null
         };
+        req.Id = this.Id ?? req.Id;
+        return req;
     }
 
 }
